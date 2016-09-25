3573a2d1972c5cd6337b1d3cbcb94954c8ce13ae
Han Wang <hanwang@dhcp-10-201-144-37.tamulink.tamu.edu>
2016-09-25 16:15:44 -0500

    upload

--- analysis/analyze.sh
+++
@@ -1,3 +0,0 @@
-R CMD BATCH src/analyze.r results/results.txt
-rm -f Rplots.pdf
-echo "Running src/analyze.r and outputting to results/results.txt (and generating figures if analyze.r has code for them)."
---
+++ public/data/README.md
@@ -0,0 +1,1 @@
+./datashift.py will rotate the current sequence used during testing and up the particpant ID.
