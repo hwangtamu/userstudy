662010a6b02f72d1ffe704499650c84d42f894b8
Han Wang <whxjtu@gmail.com>
2016-09-25 17:30:53 -0500

    roll back

--- app.js
+++ app.js
@@ -4,18 +4,15 @@
 // Example run command: `node app.js 9000 6380 true`; listen on port 9000, connect to redis on 6380, debug printing on.
 
 var express     = require('express')
-  , url         = require('url')
   , http        = require('http')
   , redis       = require('redis')
+  , redisClient
   , port        = process.argv[2] || 8000
   , rport       = process.argv[3] || 6379
   , debug       = process.argv[4] || null
 
-var redisURL = url.parse(process.env.REDISCLOUD_URL);
-
 // Database setup
-var redisClient = redis.createClient(redisURL.port, redisURL.hostname, {no_ready_check: true});
-redisClient.auth(redisURL.auth.split(":")[1]);
+redisClient = redis.createClient(rport)
 
 redisClient.on('connect', function() {
   console.log('Connected to redis.')
