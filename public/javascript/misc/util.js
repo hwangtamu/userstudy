a86b4844adcdfd82e6b9f80dc4e8a90fec464341
hwangtamu <whxjtu@gmail.com>
2017-01-30 21:50:15 -0600

    hotfix on text position of LF and FF

--- public/javascript/misc/util.js
+++ public/javascript/misc/util.js
@@ -40,7 +40,10 @@ function cell(t,g,j,k){
         .style("fill",function(){if(k==1||k==2){return "#68a7ca";}if(k==3||k==4){return "#b2d3e6";}if(k==6){return "#C5E3BF"}})
         .style("opacity",1);
     var textbox = cel.append("text").attr("id",j).attr("id","t"+j.toString());
-    textbox.attr("x",function(){if(k==1||k==2){return cx/2;}if(t.length>0){return cx*(0.02+0.48/t.length)-4;}})
+    textbox.attr("x",function(){
+        if(k==3 && (title[j%10]=="FF"||title[j%10]=="LF")){return cx/2;}
+        if(k==1||k==2){return cx/2;}
+        if(t.length>0){return cx*(0.02+0.48/t.length)-4;}})
         .attr("y",function(){if(k==2||k==4){return cy/2+28;}if(k==1||k==3||k==6){return cy/2+5;}})
         .attr("text-anchor",function(){
             if(k==3 && (title[j%10]=="FF"||title[j%10]=="LF")){return "middle";}
