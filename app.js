10b27106fd9dbdbf2def659b3737c85fca2ac680
Gurudev Ilangovan <ilangurudev@gmail.com>
2017-05-15 16:02:36 -0500

    Update app.js

--- app.js
+++ app.js
@@ -61,47 +61,6 @@ app.post('/', function handlePost(req, res) {
 })
 
 
-
-//var apiRouter = express.Router();
-var path = require('path')
-var formidable = require('express-formidable');
-var fs = require('fs');
-var mv = require('mv');
-var bodyParser = require('body-parser');
-
-//bodyparser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: false}));
-
-app.use(formidable({
-    uploadDir: '/public/'
-}));
-
-app.get('/upload',function(req,res){
-    console.log("code for uploading");
-    res.sendfile(path.join(__dirname+'/public/modules'+'/upload.html'));
-
-});
-
-app.post('/upload',function(req,res) {
-    console.log("moving code");
-    var files = req.files;
-    var oldpath = files.filetoupload.path;
-    //var newpath = "./public/" + files.filetoupload.name;
-    var newpath = "./public/" + "data_f2.csv";
-    mv(oldpath, newpath, function (err) {
-            if (err) {
-                throw err;
-            };
-    });
-    console.log('File uploaded');
-    res.write('<p>File uploaded</p> </br>');
-    res.end();
-});
-
-
-
-
 var path = require('path')
 var formidable = require('express-formidable');
 var fs = require('fs');
