6ee44656ff9f51a8a03ddfc5c8ba2655be298a54
Han <whxjtu@gmail.com>
2017-05-23 16:06:28 -0500

    meeting hotfix

--- public/javascript/misc/util.js
+++ public/javascript/misc/util.js
@@ -50,7 +50,7 @@ function cell(t,g,j,k){
         rectangle.attr("x",0).attr("y",0).attr("width",cx+6).attr("id","r"+j.toString())
             .attr("height",function(){if(k==2||k==4){return cy*2+23;}if(k==0||k==5||(index_r>0 && k==1)){return 0;}return cy;})
             .style("fill","none")
-            .style("fill",function(){if(k==1||k==2){return "#68a7ca";}if(k==3||k==4){return "#b2d3e6";}if(k==6){return "#add8e6 "}})
+            .style("fill",function(){if(k==1||k==2){return "#add8e6";}if(k==3||k==4){return "#b2d3e6";}if(k==6){return "#C5E3BF"}})
             .style("opacity",1);
     }
 
