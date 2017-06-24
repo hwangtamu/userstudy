43df282f1d4f60cee6084f047bc41362519c933a
Gurudev Ilangovan <ilangurudev@gmail.com>
2017-06-23 20:39:22 -0500

    makes all_grouped.csv as well

--- pipeline/1_preprocessing.R
+++ pipeline/1_preprocessing.R
@@ -24,6 +24,24 @@ is_not_empty = Vectorize(is_not_empty)
   select(ID, voter_reg_num, last_name, first_name, dob, race, type, 
          everything(), -name_sufx_cd,-src))
 
+org_names <- c("file_id","voter_reg_num","last_name","first_name","dob","name_sufx_cd","type","ID","src","race","answer")
+lookup_2 <- 
+  source_data %>%
+  select(ID) %>%
+  unique() %>%
+  mutate(n = 1:n())
+
+source_data_2 <- 
+  source_data %>%
+  left_join(lookup_2) %>%
+  mutate(ID = n) %>%
+  select(-n) %>%
+  mutate_each(funs(ifelse(is_not_empty(.),.,"")))
+
+names(source_data_2) <- org_names
+write_csv(source_data_2,"./data_input/all_groups_changed.csv")    
+
+
 lookup <- 
   input_data %>%
     select(ID) %>%
